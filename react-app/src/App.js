c1722457b908998238bfd3fcfdb8f8ae2bb28122
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2022-02-14 09:06:02 -0600

    react-done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,47 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  // State variables
+  const [todos, setTodos] = useState([]);
+
+  // Binding variables
+  const todoText = useRef();
+
+  // Lifecycle Hooks
+  useEffect(() => {
+    const existingTodos = localStorage.getItem("todos");
+    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
+  }, []);
+
+  function addTodo(event) {
+    event.preventDefault();
+    const next = [...todos, todoText.current.value];
+    setTodos(next);
+    localStorage.setItem("todos", JSON.stringify(next));
+  }
   return (
-    <div className="app">
-    </div>
+    <>
+      <div className="container">
+        <form onSubmit={addTodo}>
+          <input
+            type="text"
+            ref={todoText}
+            placeholder="what needs to be done?"
+            className="textInput"
+          
+          />
+          <input type="submit" value="Add ToDo" className="button" />
+        </form>
+      </div>
+      <div className="list">
+        <ul id="todos">
+          {todos.map((todo) => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
